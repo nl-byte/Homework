9007fec70457d0ed69b34f3dbf2c7825448a0576
nl-byte <lawasnichole.nl@gmail.com>
2023-09-18 23:53:00 -0400

    wk4index.html

--- Week3hw.js
+++ Week3hw.js
@@ -1,9 +1,9 @@
 // Create an array of pizzaToppings with at least four different toppings
 //outputs a pizza Object with appropriate key-value pairs for size, crust, and toppings
-const pizzaToppings = ['ham', 'cheese', 'pineapples',  '&','chicken'];
+const pizzaToppings = ['ham', 'cheese', 'pineapples','chicken'];
 //Create a greetCustomer function that prints a message that welcomes a guest, then informs them of the available toppings by looping over pizzaToppings (don't worry about perfect grammar here yet, i.e. "a, b, and c", see Bonus Challenge #9)
 function greetCustomer() {
- let greeting =  " Welcome to BertanBertie's our toppings are:";
+ let greeting =  " Welcome to BertanBertie's, our toppings are:";
 for (let topping of pizzaToppings) {
   greeting += `${topping}`;
 }
@@ -21,6 +21,10 @@ return [size,crust,toppings];
 // Create a preparePizza function
 //has an array as its parameter with three items: a size, a crust, and a list of toppings
 //prints something like "...Cooking pizza..."
+// Create a servePizza function
+//has a parameter of a pizza Object
+//logs a message that the pizza is ready and repeats the order, i.e. "Order up! Here's your large thick crust pizza with x, y, z, ... Enjoy!"
+//outputs the same pizza Object that was passed in
 
 function preparePizza([size,crust,toppings]) {
   console.log('..preparing your pizza..');
@@ -33,12 +37,6 @@ function preparePizza([size,crust,toppings]) {
 
 }
 
-
-// Create a servePizza function
-//has a parameter of a pizza Object
-//logs a message that the pizza is ready and repeats the order, i.e. "Order up! Here's your large thick crust pizza with x, y, z, ... Enjoy!"
-//outputs the same pizza Object that was passed in
-
 function servePizza(pizza) {
   console.log(`Orders ready! Here's your: ${pizza.size} ${pizza.crust} crust pizza with ${pizza.toppings}. Enjoy!`);
 }
@@ -46,7 +44,7 @@ function servePizza(pizza) {
 //Call each function and (starting with preparePizza) use the returned value from the previous function as its input
 greetCustomer();
 servePizza(preparePizza(getPizzaOrder('small','thincrust','ham','pineapple')));
-
+3
 
 
 //Add, commit, and push your JS file to your GitHub repo.
